cc4c3232a2349f0861961fd3a7577db97565215a
Hesllar Henry <hesllar.linzmayer@gmail.com>
2022-08-09 12:29:34 -0400

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,54 @@
+import { useEffect, useReducer } from "react"
+
+import { todoRedurcer } from "../08-useReducer/todoRedurcer";
+
+const initialState = [];
+
+const init = () =>{
+    return JSON.parse(localStorage.getItem('Todos')) || [];
+}
+
+export const useTodo = () => {
+
+    
+    const [todos, dispatch] = useReducer(todoRedurcer, initialState, init);
+
+      useEffect(() => {
+        localStorage.setItem('Todos',JSON.stringify(todos));
+    }, [todos]);
+
+    const handleNewTodo = (newTodo) =>{
+        const action = {
+            type:'[TODO] Add todo',
+            payload:newTodo
+        }
+
+        dispatch(action)
+    }
+
+    const handleDeleteTodo = (id) =>{
+        dispatch({
+            type:'[TODO] Remove todo',
+            payload:id
+        })
+    }
+
+    const handleToggleTodo = (id) =>{
+        dispatch({
+            type:'[TODO] Toggle todo',
+            payload:id
+        })
+    }
+
+
+    return{
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCount: todos.length,
+        pendingTodosCount:todos.filter(todo => !todo.done).length
+    }
+
+ 
+}
